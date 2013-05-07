8ab0a2dbf8ec0fb974e4e0a38562241ea2319724
snthd <158rune@gmail.com>
2013-05-07 15:27:39 +0900

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ulendar::Application.config.secret_key_base = '6e240dd8558182b1da19806a5dd130516340c3bb9816f8c7f0caa71920adbabb1cfd6fa3d2d8219122fa9dd70f65e26b124cc3e37f0cef63e9af4d2f6a1d0a5d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ulendar::Application.config.session_store :cookie_store, key: '_ulendar_session'
